4213923ddcbb7d968f98a8bb512d2a64fe20d20d
alexxio1989 <alessiopinna.elis@gmail.com>
2022-04-29 10:49:40 +0200

    first commit

---
+++ src/main/java/com/soaptest/config/WSConfig.java
@@ -0,0 +1,36 @@
+package com.soaptest.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+
+@EnableWs
+@Configuration
+public class WSConfig extends WsConfigurerAdapter{
+	
+	@Bean
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		return new ServletRegistrationBean(servlet, "/ws/*");
+	}
+	
+	@Bean(name = "soaptest")
+	public Wsdl11Definition wsdlDefinition() {
+		SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
+		wsdl11Definition.setWsdl(new ClassPathResource("/wsdl/soaptest.wsdl"));
+		return wsdl11Definition;
+	}
+
+}
---
+++ src/main/java/com/soaptest/endpoint/SoapTestEndpoint.java
@@ -0,0 +1,26 @@
+package com.soaptest.endpoint;
+
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import soap_test_schema.helloworld.HelloWorldRequest;
+import soap_test_schema.helloworld.HelloWorldResponse;
+
+
+@Endpoint
+public class SoapTestEndpoint {
+	
+	private static final String NAMESPACE_URI = "http://soap-test-schema/helloworld";
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "helloWorldRequest")
+	@ResponsePayload
+	public HelloWorldResponse getArticle(@RequestPayload HelloWorldRequest request) {
+		HelloWorldResponse response = new HelloWorldResponse();
+		
+		return response;
+	}
+
+}
